f69b17fb1289f6f2d5dbd3f79bc0b882e742098b
baileytorch <baileytorch@hotmail.com>
2025-10-13 12:23:36 -0300

    Filtrando datos desde DB

--- datos/obtener_datos.py
+++ datos/obtener_datos.py
@@ -1,5 +1,7 @@
 from datos.conexion import Session
 from sqlalchemy import func
+from modelos.comuna import Comuna
+from modelos.combustible import Combustible
 
 sesion = Session()
 
@@ -8,3 +10,30 @@ def obtener_listado_objetos(object):
     listado_objetos = sesion.query(object).all()
     if len(listado_objetos) > 0:
         return listado_objetos
+
+
+# def obtener_objeto_individual(object, dato: str):
+#     nombre_campo = any
+#     if isinstance(object, Comuna):
+#         nombre_campo = Comuna.nombre_comuna
+#     elif isinstance(object, Combustible):
+#         nombre_campo = Combustible.tipo_combustible
+
+#     objeto_especifico = sesion.query(object).filter_by(
+#         nombre_campo=dato.title()).first()
+#     if objeto_especifico:
+#         return objeto_especifico
+
+
+def obtener_comuma_individual(object, comuna: str):
+    objeto_especifico = sesion.query(object).filter_by(
+        nombre_comuna=comuna.title()).first()
+    if objeto_especifico:
+        return objeto_especifico
+
+
+def obtener_combustible_individual(object, combustible: str):
+    objeto_especifico = sesion.query(object).filter_by(
+        tipo_combustible=combustible.title()).first()
+    if objeto_especifico:
+        return objeto_especifico
--- main,.py
+++ main,.py
@@ -1,32 +1,64 @@
-from datos.obtener_datos import obtener_listado_objetos
+from datos.obtener_datos import obtener_listado_objetos, obtener_objeto_individual, obtener_combustible_individual
 from modelos.comuna import Comuna
+from modelos.combustible import Combustible
 
-objeto = Comuna
 
-listado_comunas = obtener_listado_objetos(objeto)
-if listado_comunas:
-    for comuna in listado_comunas:
-        print(f'{comuna.id} {comuna.codigo_comuna} {comuna.nombre_comuna}')
+def obtener_listado_comunas():
+    objeto = Comuna
+    listado_comunas = obtener_listado_objetos(objeto)
+    if listado_comunas:
+        for comuna in listado_comunas:
+            print(f'{comuna.id} {comuna.codigo_comuna} {comuna.nombre_comuna}')
 
-        # from iu.menu_principal import menu_principal
 
-        # def main():
-        #     while True:
-        #         print()
-        #         menu_principal()
-        #         print()
-        #         opcion = input("Seleccione su opción [0-3]: ")
+def obtener_comuna_inidividual():
+    objeto = Comuna
+    nombre_comuna = input('Ingrese nombre de comuna a buscar: ')
+    comuna_encontrada = obtener_objeto_individual(objeto, nombre_comuna)
+    if comuna_encontrada:
+        print(f'{comuna_encontrada.id} {comuna_encontrada.codigo_comuna} {comuna_encontrada.nombre_comuna}')
 
-        #         if opcion == "1":
-        #             pass
-        #         elif opcion == "2":
-        #             pass
-        #         elif opcion == "3":
-        #             pass
-        #         elif opcion == "0":
-        #             print("Saliendo del sistema...")
-        #             break
-        #         else:
-        #             print("Opción NO corresponde, ingrese nuevamente...")
 
-        # main()
+def obtener_listado_cobustibles():
+    objeto = Combustible
+    listado_combustibles = obtener_listado_objetos(objeto)
+    if listado_combustibles:
+        for combustible in listado_combustibles:
+            print(
+                f'{combustible.id} {combustible.tipo_combustible} {combustible.descripcion_tipo_combustible}')
+
+
+def obtener_combustible_inidividual():
+    objeto = Combustible
+    nombre_combustible = input('Ingrese combustible a buscar: ')
+    combustible_encontrado = obtener_objeto_individual(
+        objeto, nombre_combustible)
+    if combustible_encontrado:
+        print(f'{combustible_encontrado.id} {combustible_encontrado.tipo_combustible} {combustible_encontrado.descripcion_tipo_combustible}')
+
+
+obtener_comuna_inidividual()
+obtener_combustible_inidividual()
+
+# from iu.menu_principal import menu_principal
+
+# def main():
+#     while True:
+#         print()
+#         menu_principal()
+#         print()
+#         opcion = input("Seleccione su opción [0-3]: ")
+
+#         if opcion == "1":
+#             pass
+#         elif opcion == "2":
+#             pass
+#         elif opcion == "3":
+#             pass
+#         elif opcion == "0":
+#             print("Saliendo del sistema...")
+#             break
+#         else:
+#             print("Opción NO corresponde, ingrese nuevamente...")
+
+# main()
